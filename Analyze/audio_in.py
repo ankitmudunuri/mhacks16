802607849e470f51528dafc14f49baf69e6445e4
arnavDeol <arnavdeol1@gmail.com>
2023-11-18 18:17:29 -0500

    thuggin it out frfr

--- Analyze/audio_in.py
+++ Analyze/audio_in.py
@@ -1,7 +1,7 @@
 import queue
+import pyaudio
 import re
 import sys
-import pyaudio
 from google.cloud import storage
 from google.cloud import speech
 
@@ -10,6 +10,7 @@ from google.cloud import speech
 # Audio recording parameters
 RATE = 16000
 CHUNK = int(RATE / 10)  # 100ms
+Flag = False # This flag is used to determine if the user is done speaking
 
 
 class MicrophoneStream:
@@ -113,24 +114,6 @@ class MicrophoneStream:
 
 def listen_print_loop(responses: object) -> str:
     """Iterates through server responses and prints them.
-
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-
-    Args:
-        responses: List of server responses
-
-    Returns:
-        The transcribed text.
     """
     
     count = 100
@@ -141,49 +124,10 @@ def listen_print_loop(responses: object) -> str:
         if check == count:
             break
     
+def response_to_sentence(responses: object):
+    pass
     
-    
-    
-    # num_chars_printed = 0
-    # for response in responses:
-    #     if not response.results:
-    #         continue
 
-    #     # The `results` list is consecutive. For streaming, we only care about
-    #     # the first result being considered, since once it's `is_final`, it
-    #     # moves on to considering the next utterance.
-    #     result = response.results[0]
-    #     if not result.alternatives:
-    #         continue
-
-    #     # Display the transcription of the top alternative.
-    #     transcript = result.alternatives[0].transcript
-
-    #     # Display interim results, but with a carriage return at the end of the
-    #     # line, so subsequent lines will overwrite them.
-    #     #
-    #     # If the previous result was longer than this one, we need to print
-    #     # some extra spaces to overwrite the previous result
-    #     overwrite_chars = " " * (num_chars_printed - len(transcript))
-
-    #     if not result.is_final:
-    #         sys.stdout.write(transcript + overwrite_chars + "\r")
-    #         sys.stdout.flush()
-
-    #         num_chars_printed = len(transcript)
-
-    #     else:
-    #         print(transcript + overwrite_chars)
-
-    #         # Exit recognition if any of the transcribed phrases could be
-    #         # one of our keywords.
-    #         if re.search(r"\b(exit|quit)\b", transcript, re.I):
-    #             print("Exiting..")
-    #             break
-
-    #         num_chars_printed = 0
-
-    #     return transcript
 
 
 def main() -> None:
@@ -217,5 +161,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    # authenticate_implicit_with_adc()
     main()
